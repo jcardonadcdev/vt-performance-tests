6726f0537ee6bb3bf64f69c501cbdade1d4349bf
James Cardona <jcardona@esri.com>
2015-10-07 16:46:23 -0400

    add benchmark and requirements

---
+++ js/bench-clip.js
@@ -0,0 +1,244 @@
+var Pbf = require('../node_modules/mapbox-gl/node_modules/pbf'),
+    VectorTile = require('vector-tile').VectorTile,
+    Benchmark = require('benchmark'),
+    fs = require('fs'),
+    TilePyramid = require('../node_modules/mapbox-gl/js/source/tile_pyramid'),
+    WorkerTile = require('./worker_tile'),
+    TileCoord= require('../node_modules/mapbox-gl/js/source/tile_coord'),
+    util = require('../node_modules/mapbox-gl/js/util/util');
+
+console.log('All requires worked');
+
+var countId = 1;
+
+function createPyramid(options) {
+    return new TilePyramid(util.extend({
+        tileSize: 512,
+        minzoom: 0,
+        maxzoom: 14,
+        load: function() {},
+        abort: function() {},
+        unload: function() {},
+        add: function() {},
+        remove: function() {}
+    }, options));
+}
+
+function getChildPos(childId, parentId){
+    var tilePos = TileCoord.fromID(childId);
+    var parentPos = TileCoord.fromID(parentId);
+    var dz = tilePos.z - parentPos.z;
+    var xPos = tilePos.x & ((1 << dz) - 1);
+    var yPos = tilePos.y & ((1 << dz) - 1);
+    return {
+        dz: dz,
+        xPos: xPos,
+        yPos: yPos
+    };
+}
+
+var source = JSON.parse(fs.readFileSync(__dirname + '/../fixtures/clipping/source.json', 'utf-8'));
+var layers = source.layers;
+
+var timerun = 0;
+
+var index = JSON.parse(fs.readFileSync(__dirname + '/../fixtures/index.json', 'utf-8')).index;
+
+var pyramidIndex;
+var pyramid;
+
+var coord1 = new TileCoord(11, 354, 798, 0),
+    coord2 = new TileCoord(11, 354, 799, 0),
+    coord3 = new TileCoord(11, 355, 798, 0),
+    coord4 = new TileCoord(11, 355, 799, 0);
+
+var coord5 = new TileCoord(11, 354, 798, 0),
+    coord6 = new TileCoord(11, 354, 799, 0),
+    coord7 = new TileCoord(11, 355, 798, 0),
+    coord8 = new TileCoord(11, 355, 799, 0);
+
+var suite1 = new Benchmark.Suite(),
+    data_1_oz = fs.readFileSync(__dirname + '/../fixtures/clipping/vc-oz-10-177-399.pbf');
+
+var suite2 = new Benchmark.Suite(),
+    data_1_f1 = fs.readFileSync(__dirname + '/../fixtures/clipping/vc-f-11-354-798.pbf'),
+    data_1_f2 = fs.readFileSync(__dirname + '/../fixtures/clipping/vc-f-11-354-799.pbf'),
+    data_1_f3 = fs.readFileSync(__dirname + '/../fixtures/clipping/vc-f-11-355-798.pbf'),
+    data_1_f4 = fs.readFileSync(__dirname + '/../fixtures/clipping/vc-f-11-355-799.pbf');
+
+var suite3 = new Benchmark.Suite(),
+    data_2_oz = fs.readFileSync(__dirname + '/../fixtures/clipping/vc-oz-10-177-401.pbf');
+
+var suite4 = new Benchmark.Suite(),
+    data_2_f1 = fs.readFileSync(__dirname + '/../fixtures/clipping/vc-f-11-354-802.pbf'),
+    data_2_f2 = fs.readFileSync(__dirname + '/../fixtures/clipping/vc-f-11-354-803.pbf'),
+    data_2_f3 = fs.readFileSync(__dirname + '/../fixtures/clipping/vc-f-11-355-802.pbf'),
+    data_2_f4 = fs.readFileSync(__dirname + '/../fixtures/clipping/vc-f-11-355-803.pbf');
+
+suite1
+.add('process leaf tile 1 overzoomed tile lvl 11', function() {
+    readTile(data_1_oz, layers, coord1, true);
+    readTile(data_1_oz, layers, coord2, true);
+    readTile(data_1_oz, layers, coord3, true);
+    readTile(data_1_oz, layers, coord4, true);
+})
+.on('start', function(){
+    timerun = 0;
+    pyramidIndex = createPyramid({
+        index: index
+    });
+    pyramid = createPyramid();
+    console.log("\nSUITE 1");
+})
+.on('cycle', function(event) {
+    console.log('\n**' + event.target.name);
+    console.log('**** num samples' + ": " + event.target.stats.sample.length);
+    var total = 0;
+    for(var i = 0; i < event.target.stats.sample.length; i++){
+        total += event.target.stats.sample[i];
+    }
+    //console.log("**** timerun: ", timerun);
+    console.log("**** time per run: ", timerun / event.target.stats.sample.length);
+    console.log("**** avg time from samples: ", total / event.target.stats.sample.length);
+    console.log("**** num processes per second: ", String(event.target) + '\n');
+})
+.run();
+
+suite2
+.add('process full tiles 1 lvl 11', function() {
+    readTile(data_1_f1, layers, coord1, false);
+    readTile(data_1_f2, layers, coord2, false);
+    readTile(data_1_f3, layers, coord3, false);
+    readTile(data_1_f4, layers, coord4, false);
+})
+.on('start', function(){
+    timerun = 0;
+    pyramidIndex = createPyramid({
+        index: index
+    });
+    pyramid = createPyramid();
+    console.log("\nSUITE 2");
+})
+.on('cycle', function(event) {
+    console.log('\n**' + event.target.name);
+    console.log('**** num samples' + ": " + event.target.stats.sample.length);
+    var total = 0;
+    for(var i = 0; i < event.target.stats.sample.length; i++){
+        total += event.target.stats.sample[i];
+    }
+    //console.log("**** timerun: ", timerun);
+    console.log("**** time per run: ", timerun / event.target.stats.sample.length);
+    console.log("**** avg time from samples: ", total / event.target.stats.sample.length);
+    console.log("**** num processes per second: ", String(event.target) + '\n');
+})
+.run();
+
+suite3
+    .add('process leaf tile 2 overzoomed tile lvl 11', function() {
+        readTile(data_2_oz, layers, coord5, true);
+        readTile(data_2_oz, layers, coord6, true);
+        readTile(data_2_oz, layers, coord7, true);
+        readTile(data_2_oz, layers, coord8, true);
+    })
+    .on('start', function(){
+        timerun = 0;
+        pyramidIndex = createPyramid({
+            index: index
+        });
+        pyramid = createPyramid();
+        console.log("\nSUITE 3");
+    })
+    .on('cycle', function(event) {
+        console.log('\n**' + event.target.name);
+        console.log('**** num samples' + ": " + event.target.stats.sample.length);
+        var total = 0;
+        for(var i = 0; i < event.target.stats.sample.length; i++){
+            total += event.target.stats.sample[i];
+        }
+        //console.log("**** timerun: ", timerun);
+        console.log("**** time per run: ", timerun / event.target.stats.sample.length);
+        console.log("**** avg time from samples: ", total / event.target.stats.sample.length);
+        console.log("**** num processes per second: ", String(event.target) + '\n');
+    })
+    .run();
+
+suite4
+    .add('process full tiles 2 lvl 11', function() {
+        readTile(data_2_f1, layers, coord5, false);
+        readTile(data_2_f2, layers, coord6, false);
+        readTile(data_2_f3, layers, coord7, false);
+        readTile(data_2_f4, layers, coord8, false);
+    })
+    .on('start', function(){
+        timerun = 0;
+        pyramidIndex = createPyramid({
+            index: index
+        });
+        pyramid = createPyramid();
+        console.log("\nSUITE 4");
+    })
+    .on('cycle', function(event) {
+        console.log('\n**' + event.target.name);
+        console.log('**** num samples' + ": " + event.target.stats.sample.length);
+        var total = 0;
+        for(var i = 0; i < event.target.stats.sample.length; i++){
+            total += event.target.stats.sample[i];
+        }
+        //console.log("**** timerun: ", timerun);
+        console.log("**** time per run: ", timerun / event.target.stats.sample.length);
+        console.log("**** avg time from samples: ", total / event.target.stats.sample.length);
+        console.log("**** num processes per second: ", String(event.target) + '\n');
+    })
+    .run();
+
+
+function readTile(data, layers, tileCoord, useIndex){
+    countId++;
+    var start = Date.now();
+
+    //make VectorTile - it contains layer info for tile
+    var buf = new Pbf(data),
+        vt = new VectorTile(buf);
+
+    var dz = 0,
+        xPos = 0,
+        yPos = 0;
+
+    //add coord to pyramid so parentId set
+    var pyramidTile;
+    if (useIndex){
+        pyramidTile = pyramidIndex.addTile(tileCoord);
+    }
+    else{
+        pyramidTile = pyramid.addTile(tileCoord);
+    }
+
+    if (pyramidTile.parentId){
+        var childPos = getChildPos(tileCoord.id, pyramidTile.parentId);
+        dz = childPos.dz;
+        xPos = childPos.xPos;
+        yPos = childPos.yPos;
+    }
+
+    var worker = new WorkerTile({
+        angle: 0,
+        collisionDebug: false,
+        coord: tileCoord,
+        maxZoom: 22,
+        overscaling: 1,
+        pitch: 0,
+        source: "esri",
+        tileSize: 512,
+        uid: countId,
+        url: "",
+        zoom: tileCoord.z
+    });
+
+    var actor = {
+        send: function(){}
+    };
+
+    worker.parse(vt, layers, actor, function(){}, dz, xPos, yPos);
+    var end = Date.now();
+    timerun += (end - start);
+};
---
+++ js/worker_tile.js
@@ -0,0 +1,458 @@
+'use strict';
+
+var FeatureTree = require('../node_modules/mapbox-gl/js/data/feature_tree');
+var CollisionTile = require('../node_modules/mapbox-gl/js/symbol/collision_tile');
+var BufferSet = require('../node_modules/mapbox-gl/js/data/buffer/buffer_set');
+var createBucket = require('../node_modules/mapbox-gl/js/data/create_bucket');
+
+module.exports = WorkerTile;
+
+function WorkerTile(params) {
+    this.coord = params.coord;
+    this.uid = params.uid;
+    this.zoom = params.zoom;
+    this.maxZoom = params.maxZoom;
+    this.tileSize = params.tileSize;
+    this.source = params.source;
+    this.overscaling = params.overscaling;
+    this.angle = params.angle;
+    this.pitch = params.pitch;
+    this.collisionDebug = params.collisionDebug;
+
+    this.stacks = {};
+}
+
+WorkerTile.prototype.parse2 = function(data, layers, actor, callback, dz, xPos, yPos) {
+
+    this.status = 'parsing';
+
+    this.featureTree = new FeatureTree(this.coord, this.overscaling);
+
+    var i, k,
+        tile = this,
+        layer,
+        bucket,
+        buffers = new BufferSet(),
+        collisionTile = new CollisionTile(this.angle, this.pitch),
+        buckets = {},
+        bucketsInOrder = this.bucketsInOrder = [],
+        bucketsBySourceLayer = {};
+
+    // Map non-ref layers to buckets.
+    for (i = 0; i < layers.length; i++) {
+        layer = layers[i];
+
+        if (layer.source !== this.source)
+            continue;
+
+        if (layer.ref)
+            continue;
+
+        var minzoom = layer.minzoom;
+        if (minzoom && this.zoom < minzoom && minzoom < this.maxZoom)
+            continue;
+
+        var maxzoom = layer.maxzoom;
+        if (maxzoom && this.zoom >= maxzoom)
+            continue;
+
+        var visibility = layer.layout.visibility;
+        if (visibility === 'none')
+            continue;
+
+        bucket = createBucket(layer, buffers, this.zoom, this.overscaling, this.collisionDebug);
+        bucket.layers = [layer.id];
+
+        buckets[bucket.id] = bucket;
+        bucketsInOrder.push(bucket);
+
+        if (data.layers) {
+            // vectortile
+            var sourceLayer = layer['source-layer'];
+            if (!bucketsBySourceLayer[sourceLayer])
+                bucketsBySourceLayer[sourceLayer] = {};
+            bucketsBySourceLayer[sourceLayer][bucket.id] = bucket;
+        } else {
+            // geojson tile
+            bucketsBySourceLayer[bucket.id] = bucket;
+        }
+    }
+
+    // Index ref layers.
+    for (i = 0; i < layers.length; i++) {
+        layer = layers[i];
+
+        if (layer.source !== this.source)
+            continue;
+
+        if (!layer.ref)
+            continue;
+
+        bucket = buckets[layer.ref];
+        if (!bucket)
+            continue;
+
+        bucket.layers.push(layer.id);
+    }
+
+    var extent = 4096;
+
+    // read each layer, and sort its features into buckets
+    if (data.layers) {
+        // vectortile
+        for (k in bucketsBySourceLayer) {
+            layer = data.layers[k];
+            if (!layer) continue;
+            if (layer.extent) extent = layer.extent;
+            sortLayerIntoBuckets(layer, bucketsBySourceLayer[k], dz, xPos, yPos);
+        }
+    } else {
+        // geojson
+        sortLayerIntoBuckets(data, bucketsBySourceLayer);
+    }
+
+    function sortLayerIntoBuckets(layer, buckets, dz, xPos, yPos) {
+        for (var i = 0; i < layer.length; i++) {
+            var feature = layer.feature(i);
+
+            // propagate clipped position in tile at the feature level
+            feature.dz = dz;
+            feature.xPos = xPos;
+            feature.yPos = yPos;
+
+            for (var key in buckets) {
+                var bucket = buckets[key];
+                if (bucket.filter(feature)) {
+                    bucket.features.push(feature);
+                }
+            }
+        }
+    }
+
+    var prevPlacementBucket;
+    var remaining = bucketsInOrder.length;
+
+    for (i = bucketsInOrder.length - 1; i >= 0; i--) {
+        bucket = bucketsInOrder[i];
+        parseBucket(tile, bucket);
+        /*// immediately parse buckets where order doesn't matter and no dependencies
+        if (!bucket.needsPlacement && !bucket.getDependencies) {
+            parseBucket(tile, bucket);
+        }*/
+    }
+
+    function parseBucket(tile, bucket, skip) {
+        //if (bucket.getDependencies && !bucket.dependenciesLoaded) return;
+        //if (bucket.needsPlacement && !bucket.previousPlaced) return;
+
+        if (!skip) {
+            var now = Date.now();
+            if (bucket.features.length) bucket.addFeatures(collisionTile);
+            var time = Date.now() - now;
+            /*if (bucket.interactive) {
+                for (var i = 0; i < bucket.features.length; i++) {
+                    var feature = bucket.features[i];
+                    tile.featureTree.insert(feature.bbox(), bucket.layers, feature);
+                }
+            }*/
+            /*if (typeof self !== 'undefined') {
+                self.bucketStats = self.bucketStats || {_total: 0};
+                self.bucketStats._total += time;
+                self.bucketStats[bucket.id] = (self.bucketStats[bucket.id] || 0) + time;
+            }*/
+        }
+
+        /*remaining--;
+
+        if (!remaining) {
+            done();
+            return;
+        }
+
+        // try parsing the next bucket, if it is ready
+        if (bucket.next) {
+            bucket.next.previousPlaced = true;
+            parseBucket(tile, bucket.next);
+        }*/
+    }
+
+    function done() {
+
+        tile.status = 'done';
+
+        if (tile.redoPlacementAfterDone) {
+            var result = tile.redoPlacement(tile.angle, tile.pitch).result;
+            buffers.glyphVertex = result.buffers.glyphVertex;
+            buffers.iconVertex = result.buffers.iconVertex;
+            buffers.collisionBoxVertex = result.buffers.collisionBoxVertex;
+        }
+
+        var transferables = [],
+            elementGroups = {};
+
+        for (k in buffers) {
+            transferables.push(buffers[k].array);
+        }
+
+        for (k in buckets) {
+            elementGroups[k] = buckets[k].elementGroups;
+        }
+
+        callback(null, {
+            elementGroups: elementGroups,
+            buffers: buffers,
+            extent: extent
+        }, transferables);
+    }
+};
+
+WorkerTile.prototype.parse = function(data, layers, actor, callback, dz, xPos, yPos) {
+
+    this.status = 'parsing';
+
+    this.featureTree = new FeatureTree(this.coord, this.overscaling);
+
+    var i, k,
+        tile = this,
+        layer,
+        bucket,
+        buffers = new BufferSet(),
+        collisionTile = new CollisionTile(this.angle, this.pitch),
+        buckets = {},
+        bucketsInOrder = this.bucketsInOrder = [],
+        bucketsBySourceLayer = {};
+
+    // Map non-ref layers to buckets.
+    for (i = 0; i < layers.length; i++) {
+        layer = layers[i];
+
+        if (layer.source !== this.source)
+            continue;
+
+        if (layer.ref)
+            continue;
+
+        var minzoom = layer.minzoom;
+        if (minzoom && this.zoom < minzoom && minzoom < this.maxZoom)
+            continue;
+
+        var maxzoom = layer.maxzoom;
+        if (maxzoom && this.zoom >= maxzoom)
+            continue;
+
+        var visibility = layer.layout.visibility;
+        if (visibility === 'none')
+            continue;
+
+        bucket = createBucket(layer, buffers, this.zoom, this.overscaling, this.collisionDebug);
+        bucket.layers = [layer.id];
+
+        buckets[bucket.id] = bucket;
+        bucketsInOrder.push(bucket);
+
+        if (data.layers) {
+            // vectortile
+            var sourceLayer = layer['source-layer'];
+            if (!bucketsBySourceLayer[sourceLayer])
+                bucketsBySourceLayer[sourceLayer] = {};
+            bucketsBySourceLayer[sourceLayer][bucket.id] = bucket;
+        } else {
+            // geojson tile
+            bucketsBySourceLayer[bucket.id] = bucket;
+        }
+    }
+
+    // Index ref layers.
+    for (i = 0; i < layers.length; i++) {
+        layer = layers[i];
+
+        if (layer.source !== this.source)
+            continue;
+
+        if (!layer.ref)
+            continue;
+
+        bucket = buckets[layer.ref];
+        if (!bucket)
+            continue;
+
+        bucket.layers.push(layer.id);
+    }
+
+    var extent = 4096;
+
+    // read each layer, and sort its features into buckets
+    if (data.layers) {
+        // vectortile
+        for (k in bucketsBySourceLayer) {
+            layer = data.layers[k];
+            if (!layer) continue;
+            if (layer.extent) extent = layer.extent;
+            sortLayerIntoBuckets(layer, bucketsBySourceLayer[k], dz, xPos, yPos);
+        }
+    } else {
+        // geojson
+        sortLayerIntoBuckets(data, bucketsBySourceLayer);
+    }
+
+    function sortLayerIntoBuckets(layer, buckets, dz, xPos, yPos) {
+        for (var i = 0; i < layer.length; i++) {
+          var feature = layer.feature(i);
+
+            // propagate clipped position in tile at the feature level
+            feature.dz = dz;
+            feature.xPos = xPos;
+            feature.yPos = yPos;
+
+            for (var key in buckets) {
+                var bucket = buckets[key];
+                if (bucket.filter(feature)) {
+                    bucket.features.push(feature);
+                }
+            }
+        }
+    }
+
+    var prevPlacementBucket;
+    var remaining = bucketsInOrder.length;
+
+    /*
+     *  The async parsing here is a bit tricky.
+     *  Some buckets depend on resources that may need to be loaded async (glyphs).
+     *  Some buckets need to be parsed in order (to get collision priorities right).
+     *
+     *  Dependencies calls are initiated first to get those rolling.
+     *  Buckets that don't need to be parsed in order, aren't to save time.
+     */
+
+    for (i = bucketsInOrder.length - 1; i >= 0; i--) {
+        bucket = bucketsInOrder[i];
+
+        // Link buckets that need to be parsed in order
+        if (bucket.needsPlacement) {
+            if (prevPlacementBucket) {
+                prevPlacementBucket.next = bucket;
+            } else {
+                bucket.previousPlaced = true;
+            }
+            prevPlacementBucket = bucket;
+        }
+
+        if (bucket.getDependencies) {
+            bucket.getDependencies(this, actor, dependenciesDone(bucket));
+        }
+
+        // immediately parse buckets where order doesn't matter and no dependencies
+        if (!bucket.needsPlacement && !bucket.getDependencies) {
+            parseBucket(tile, bucket);
+        }
+    }
+
+    function dependenciesDone(bucket) {
+        return function(err) {
+            bucket.dependenciesLoaded = true;
+            parseBucket(tile, bucket, err);
+        };
+    }
+
+    function parseBucket(tile, bucket, skip) {
+        if (bucket.getDependencies && !bucket.dependenciesLoaded) return;
+        if (bucket.needsPlacement && !bucket.previousPlaced) return;
+
+        if (!skip) {
+            var now = Date.now();
+            if (bucket.features.length) bucket.addFeatures(collisionTile);
+            var time = Date.now() - now;
+            if (bucket.interactive) {
+                for (var i = 0; i < bucket.features.length; i++) {
+                    var feature = bucket.features[i];
+                    tile.featureTree.insert(feature.bbox(), bucket.layers, feature);
+                }
+            }
+            if (typeof self !== 'undefined') {
+                self.bucketStats = self.bucketStats || {_total: 0};
+                self.bucketStats._total += time;
+                self.bucketStats[bucket.id] = (self.bucketStats[bucket.id] || 0) + time;
+            }
+        }
+
+        remaining--;
+
+        if (!remaining) {
+            done();
+            return;
+        }
+
+        // try parsing the next bucket, if it is ready
+        if (bucket.next) {
+            bucket.next.previousPlaced = true;
+            parseBucket(tile, bucket.next);
+        }
+    }
+
+    function done() {
+
+        tile.status = 'done';
+
+        if (tile.redoPlacementAfterDone) {
+            var result = tile.redoPlacement(tile.angle, tile.pitch).result;
+            buffers.glyphVertex = result.buffers.glyphVertex;
+            buffers.iconVertex = result.buffers.iconVertex;
+            buffers.collisionBoxVertex = result.buffers.collisionBoxVertex;
+        }
+
+        var transferables = [],
+            elementGroups = {};
+
+        for (k in buffers) {
+            transferables.push(buffers[k].array);
+        }
+
+        for (k in buckets) {
+            elementGroups[k] = buckets[k].elementGroups;
+        }
+
+        callback(null, {
+            elementGroups: elementGroups,
+            buffers: buffers,
+            extent: extent
+        }, transferables);
+    }
+};
+
+WorkerTile.prototype.redoPlacement = function(angle, pitch, collisionDebug) {
+
+    if (this.status !== 'done') {
+        this.redoPlacementAfterDone = true;
+        this.angle = angle;
+        return {};
+    }
+
+    var buffers = new BufferSet();
+    var transferables = [];
+    var elementGroups = {};
+    var collisionTile = new CollisionTile(angle, pitch);
+
+    var bucketsInOrder = this.bucketsInOrder;
+    for (var i = bucketsInOrder.length - 1; i >= 0; i--) {
+        var bucket = bucketsInOrder[i];
+
+        if (bucket.type === 'symbol') {
+            bucket.placeFeatures(collisionTile, buffers, collisionDebug);
+            elementGroups[bucket.id] = bucket.elementGroups;
+        }
+    }
+
+    for (var k in buffers) {
+        transferables.push(buffers[k].array);
+    }
+
+    return {
+        result: {
+            elementGroups: elementGroups,
+            buffers: buffers
+        },
+        transferables: transferables
+    };
+
+};
